1cf05d2b3f7cc3bc523175a98d8808eafb47d0c6
mylene <m.goberville@europamiante.com>
2022-03-29 13:46:04 +0200

    initialisation des tables

---
+++ src/Entity/Trajet.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TrajetRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TrajetRepository::class)]
+class Trajet
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $depart;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $arrivee;
+
+    #[ORM\Column(type: 'date')]
+    private $date;
+
+    #[ORM\Column(type: 'time')]
+    private $heure;
+
+    #[ORM\Column(type: 'integer')]
+    private $place;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $escale;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDepart(): ?string
+    {
+        return $this->depart;
+    }
+
+    public function setDepart(string $depart): self
+    {
+        $this->depart = $depart;
+
+        return $this;
+    }
+
+    public function getArrivee(): ?string
+    {
+        return $this->arrivee;
+    }
+
+    public function setArrivee(string $arrivee): self
+    {
+        $this->arrivee = $arrivee;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getHeure(): ?\DateTimeInterface
+    {
+        return $this->heure;
+    }
+
+    public function setHeure(\DateTimeInterface $heure): self
+    {
+        $this->heure = $heure;
+
+        return $this;
+    }
+
+    public function getPlace(): ?int
+    {
+        return $this->place;
+    }
+
+    public function setPlace(int $place): self
+    {
+        $this->place = $place;
+
+        return $this;
+    }
+
+    public function getEscale(): ?string
+    {
+        return $this->escale;
+    }
+
+    public function setEscale(?string $escale): self
+    {
+        $this->escale = $escale;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Voiture.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\VoitureRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: VoitureRepository::class)]
+class Voiture
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $marque;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $modele;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $couleur;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $immatriculation;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $distinction;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getMarque(): ?string
+    {
+        return $this->marque;
+    }
+
+    public function setMarque(string $marque): self
+    {
+        $this->marque = $marque;
+
+        return $this;
+    }
+
+    public function getModele(): ?string
+    {
+        return $this->modele;
+    }
+
+    public function setModele(string $modele): self
+    {
+        $this->modele = $modele;
+
+        return $this;
+    }
+
+    public function getCouleur(): ?string
+    {
+        return $this->couleur;
+    }
+
+    public function setCouleur(string $couleur): self
+    {
+        $this->couleur = $couleur;
+
+        return $this;
+    }
+
+    public function getImmatriculation(): ?string
+    {
+        return $this->immatriculation;
+    }
+
+    public function setImmatriculation(string $immatriculation): self
+    {
+        $this->immatriculation = $immatriculation;
+
+        return $this;
+    }
+
+    public function getDistinction(): ?string
+    {
+        return $this->distinction;
+    }
+
+    public function setDistinction(?string $distinction): self
+    {
+        $this->distinction = $distinction;
+
+        return $this;
+    }
+}
